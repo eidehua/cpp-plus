696283e74a9ee3cc07b0be940cda053ea32d27a6
Ed <eidehua@users.noreply.github.com>
2016-05-29 21:46:29 -0700

    Update templating.cpp

--- templating.cpp
+++ templating.cpp
@@ -1,1 +1,3 @@
 
+// temptating is a powerful concept. I will explain the general concept of templating and the implementation in c++. 
+// I will also talk a bit about Java generics and how different it is from cpp templating 
