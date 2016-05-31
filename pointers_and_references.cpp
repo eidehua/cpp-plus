0cd2360e3a22f76f041d1bdaf8cf240ece01facd
Ed <eidehua@users.noreply.github.com>
2016-05-30 18:21:22 -0700

    Create pointers_and_references.cpp

---
+++ pointers_and_references.cpp
@@ -0,0 +1,2 @@
+//https://www3.ntu.edu.sg/home/ehchua/programming/cpp/cp4_PointerReference.html
+//http://stackoverflow.com/questions/57483/what-are-the-differences-between-a-pointer-variable-and-a-reference-variable-in/57492#57492
