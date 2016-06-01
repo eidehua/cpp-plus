6adc0b22f2fb50695f5c9a0aac9586a2c84c279c
Ed <eidehua@users.noreply.github.com>
2016-05-31 23:31:32 -0700

    Update templating.cpp

--- templating.cpp
+++ templating.cpp
@@ -1,3 +1,9 @@
 
 // temptating is a powerful concept. I will explain the general concept of templating and the implementation in c++. 
 // I will also talk a bit about Java generics and how different it is from cpp templating 
+
+
+
+// Advanced topics
+
+// Curiosly recurring template patern (CRTP) https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
