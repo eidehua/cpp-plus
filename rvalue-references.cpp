8506371afb51911785e573e3cb4e48389f9ecc83
Ed <eidehua@users.noreply.github.com>
2016-06-27 23:53:57 -0700

    Update rvalue-references.cpp

--- rvalue-references.cpp
+++ rvalue-references.cpp
@@ -2,4 +2,4 @@
 //rvalue references
 
   //std::move()
-  http://stackoverflow.com/questions/3413470/what-is-stdmove-and-when-should-it-be-used
+  //http://stackoverflow.com/questions/3413470/what-is-stdmove-and-when-should-it-be-used
